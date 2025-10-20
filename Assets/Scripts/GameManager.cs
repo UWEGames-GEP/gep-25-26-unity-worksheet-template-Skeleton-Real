5ddc0895dfc92f824b02c971837fdd9bbcba4fc9
Alec <Alec2.Bloomfield@live.uwe.ac.uk>
2025-10-20 16:27:10 +0100

    Pause Function

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+    public state gameState = state.GAMEPLAY;
+
+    bool stateChangeFrame = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //state gameState = state.GAMEPLAY;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameState == state.GAMEPLAY)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                gameState = state.PAUSE;
+                stateChangeFrame = true;
+            }
+        }
+        else if (gameState == state.PAUSE)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                gameState = state.GAMEPLAY;
+                stateChangeFrame = true;
+            }
+        }
+        }
+
+    private void LateUpdate()
+    {
+        if (stateChangeFrame)
+        {
+            stateChangeFrame = false;
+            if (gameState == state.GAMEPLAY)
+                Time.timeScale = 1.0f;
+            if (gameState == state.PAUSE)
+                Time.timeScale = 0.0f;
+        }
+    }
+}
+
+public enum state
+        {
+            GAMEPLAY,
+            PAUSE
+        }
